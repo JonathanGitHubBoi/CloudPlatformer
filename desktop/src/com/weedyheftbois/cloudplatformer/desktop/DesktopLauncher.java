94a3b88e69b28141d326a0403007a5c0f79c4e62
gresh113 <40521807+gresh113@users.noreply.github.com>
2021-06-21 13:14:24 -0400

    First commit

---
+++ desktop/src/com/weedyheftbois/cloudplatformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.weedyheftbois.cloudplatformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.weedyheftbois.cloudplatformer.CloudGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CloudGame(), config);
+	}
+}
